1de7fabec51e7e6996e90cdb3e5976b2d402969b
sGowthamhacker <gowlearner04@gmail.com>
2026-04-07 09:54:28 +0530

    fix: force redeploy fresh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿from fastapi import FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
